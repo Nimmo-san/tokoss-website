123f04014140bcaafeb8f7c63c2e91c017d7a912
Nimmo-san <nat25oloman@gmail.com>
2025-06-12 22:38:45 +0100

    add: responsive mobile hamburger menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 // animating the fade, zoom, slide. triggered by scroll, not yet used
 const animatedElements = document.querySelectorAll('.animate-fade, .animate-slide, .animate-zoom');
 
+// nav toggle
+const navToggle = document.getElementById('navToggle');
+const navLinks = document.getElementById('navLinks');
+
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
@@ -11,3 +15,10 @@ const observer = new IntersectionObserver(entries => {
 }, { threshold: 0.1 });
 
 animatedElements.forEach(el => observer.observe(el));
+
+
+navToggle.addEventListener('click', () => {
+  navToggle.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
+
